6e0fefcc7f6f053a5545bcd1df659bce23572442
kyle-barth <kylebarth@me.com>
2018-08-30 15:40:40 +0100

    Challenge 1

---
+++ Challenge 1/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $.get(
+        'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus', {
+            maxResults: 5,
+            playlistId: 'UUTI5S0PqpgB0DbYgcgRU6QQ',
+            key: API_KEY // Create a .env file with: API_KEY = 'Your API Key here' in the js folder
+        },
+        function(data) {
+            $.each(data.items, function(i, item) {
+                console.log(item)
+                output = `
+                    <div class="content-block">
+                        <h2>` 
+                            + item.snippet.title + 
+                        `</h2>
+                        <div class="video-container">
+                            <iframe class="video" frameBorder="0" src="https://www.youtube.com/embed/`
+                                + item.contentDetails.videoId +
+                            `"></iframe>
+                        </div>
+                    </div>`
+                
+                $('#results').append(output)
+            })
+        }
+    )
+})
